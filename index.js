c5ba9dcf8628269d2586d20b357788d665fc927c
WesleyZala <wesley.zala.rodrigues@gmail.com>
2021-05-27 20:38:07 -0300

    Terceiro Commit

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ app.use(express.json());
 const port = 3000;
 
 const usuarioFernando = {
-    id = 1,
+    id: 1,
     nome: 'Fernando',
     idade: 35
 }
 
 const usuarios = [usuarioFernando];
 
-app.get('/usuario', (req, res)
-=> {
+app.get('/usuario', (req, res) => {
     res.json(usuarios);
 });
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
-    
+
 })
